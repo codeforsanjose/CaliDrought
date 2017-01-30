c409851ef1792aaf9d676aec1d2b4e966ae9deda
Tyler Hampton <dr.frankinfurter@gmail.com>
2017-01-30 06:07:47 -0800

    ISSUE-#1: getting started on the crawler

---
+++ lib/crawler/cdec.ex
@@ -0,0 +1,18 @@
+defmodule Calidrought.Crawler.Cdec do
+  use GenServer
+  require Logger
+
+  def start_link do
+    queue = initial_queue()
+    GenServer.start_link(__MODULE__, queue)
+  end
+
+  def init(queue) do
+    schedule_work()
+    {:ok, queue}
+  end
+
+  defp schedule_work do
+    Process.send_after(self(), :work, 1 * 1 * 100)
+  end
+end
